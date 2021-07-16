4a8e95767ed18812184444c7b066a36c3d33b8db
moizez <moizezhenrique@gmail.com>
2021-07-16 20:40:30 -0300

    ajustes

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components'
 
-export const Template = styled.div`
-    
-`;
-
 export const Body = styled.div`
     max-width: 960px;
     min-height: 70vh;
     margin: auto;
     padding: 30px;
+    background-color: #FFF;
 `;
 
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import App from './App';
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
-/*ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);*/
